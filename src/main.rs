95160c868671a25fb451b105e8d026e8fd944c01
masara <ooua815@gmail.com>
2026-08-20 14:13:47 +0700

    clean up

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,6 @@ use std::env;
 
 fn main() {
     let argv = env::args().collect::<Vec<String>>();
-
     let mut table: testcases::TestCasesVector = testcases::load_test_cases();
     parser::arguments_handler(&argv, &mut table);
 }
