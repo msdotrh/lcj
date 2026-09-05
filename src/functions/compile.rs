18a2fd34dd38287b7158cdfa804673d8e3e30b1e
masara <ooua815@gmail.com>
2026-09-05 20:21:10 +0700

    lcj can now compile c files

--- src/functions/compile.rs
+++ src/functions/compile.rs
@@ -22,7 +22,7 @@ pub fn compile_file(file: &Path, extension: &str) -> Option<PathBuf> {
     }
 }
 
-pub fn matching(extension: &str) -> FileType {
+fn matching(extension: &str) -> FileType {
     match extension {
         "cpp" | "cxx" | "c++" | "cc" => FileType::CPP,
         "c" => FileType::C,
@@ -150,6 +150,7 @@ fn compile_file_cpp(file: &Path) -> Option<PathBuf> {
         .arg("-Wall")
         .arg("-Wextra")
         .arg("-o")
+        .arg("-stdc++=23")
         .arg(&target_binary)
         .output();
 
@@ -161,7 +162,6 @@ fn compile_file_cpp(file: &Path) -> Option<PathBuf> {
     } else {
         println!("Can not compile {}!", file.to_string_lossy());
     }
-    println!("STDOUT / STDERR of {:?}:", output_of_compilation.as_ref());
     println!(
         "STDOUT: {}",
         String::from_utf8_lossy(&output_of_compilation.as_ref().unwrap().stdout)
@@ -178,5 +178,51 @@ fn compile_file_cpp(file: &Path) -> Option<PathBuf> {
 }
 
 fn compile_file_c(file: &Path) -> Option<PathBuf> {
-    todo!();
+    let file_name = file.file_name();
+    if file_name.is_none() {
+        println!(
+            "C source code in path {} do not have a name! Can not compile!",
+            path::absolute(file).unwrap().to_string_lossy()
+        );
+        return None;
+    }
+
+    let target_binary = match std::env::consts::OS {
+        "windows" => file.with_extension("exe"),
+        "linux" => file.with_extension("out"),
+        _ => {
+            panic!("The program doesn't support your OS")
+        }
+    };
+
+    let compiler = path_checking_c().unwrap();
+    let output_of_compilation = std::process::Command::new(&compiler)
+        .arg(file)
+        .arg("-Wall")
+        .arg("-Wextra")
+        .arg("-o")
+        .arg(&target_binary)
+        .output();
+
+    let status = output_of_compilation.as_ref().unwrap().status;
+    let _status_code = status.code().unwrap();
+
+    if status.success() {
+        println!("Successfully compiled!");
+    } else {
+        println!("Can not compile {}!", file.to_string_lossy());
+    }
+    println!(
+        "STDOUT: {}",
+        String::from_utf8_lossy(&output_of_compilation.as_ref().unwrap().stdout)
+    );
+    println!(
+        "STDERR: {}",
+        String::from_utf8_lossy(&output_of_compilation.as_ref().unwrap().stderr)
+    );
+
+    match status.success() {
+        true => Some(target_binary),
+        _ => None,
+    }
 }
